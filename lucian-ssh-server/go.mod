5c67e585ec08da49aaf3fbe0e6bb88ddd823d705
jsfraz <josefraz@seznam.cz>
2024-02-16 21:01:39 +0100

    Postgres database.

--- lucian-ssh-server/go.mod
+++ lucian-ssh-server/go.mod
@@ -1,7 +1,21 @@
-module lucian-ssh-server
+module jsfraz/lucian-ssh-server
 
 go 1.22.0
 
-require golang.org/x/crypto v0.19.0
+require (
+	golang.org/x/crypto v0.19.0
+	gorm.io/driver/postgres v1.5.6
+	gorm.io/gorm v1.25.7
+)
 
-require golang.org/x/sys v0.17.0 // indirect
+require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect
+	github.com/jackc/pgx/v5 v5.5.3 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
--- lucian-ssh-server/main.go
+++ lucian-ssh-server/main.go
@@ -4,25 +4,67 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
+	"encoding/json"
 	"encoding/pem"
+	"fmt"
+	"io"
 	"log"
 	"net"
+	"net/http"
 	"os"
+	"time"
 
 	"golang.org/x/crypto/ssh"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
-// https://docs.freeipapi.com/
-
 func main() {
+	// Logging
 	log.SetPrefix("lucian-ssh-server: ")
 	log.SetFlags(log.LstdFlags | log.LUTC | log.Lmicroseconds)
 
+	// Waiting for database
+	time.Sleep(time.Second * 5)
+
+	// Database
+	connStr := "postgresql://" + os.Getenv("POSTGRES_USER") + ":" + os.Getenv("POSTGRES_PASSWORD") + "@" + os.Getenv("POSTGRES_SERVER") + ":" + os.Getenv("POSTGRES_PORT") + "/" + os.Getenv("POSTGRES_DB")
+	postgres, err := gorm.Open(postgres.Open(connStr), &gorm.Config{Logger: logger.Default.LogMode(logger.Error)})
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Database schema migration
+	err = postgres.AutoMigrate(&Connection{})
+	if err != nil {
+		log.Fatal(err)
+	}
 	// Server config with password callback denying everything
 	config := &ssh.ServerConfig{
 		PasswordCallback: func(conn ssh.ConnMetadata, password []byte) (*ssh.Permissions, error) {
+			timestamp := time.Now()
 			log.Printf("Unsuccessful login attempt from %s by user '%s' with password '%s'.", conn.RemoteAddr(), conn.User(), password)
-			// TODO database
+			// Split IP and port
+			host, _, err := net.SplitHostPort(conn.RemoteAddr().String())
+			if err != nil {
+				log.Println(err)
+				return nil, ssh.ErrNoAuth
+			}
+			// Get IP data
+			json, err := getIpInfo(host)
+			if err != nil {
+				log.Println(err)
+				return nil, ssh.ErrNoAuth
+			}
+			connection, err := NewConnection(*json, conn.User(), string(password), timestamp)
+			if err != nil {
+				log.Println(err)
+				return nil, ssh.ErrNoAuth
+			}
+			// Upload to database
+			if err := postgres.Create(&connection).Error; err != nil {
+				log.Println(err)
+			}
 			return nil, ssh.ErrNoAuth
 		},
 	}
@@ -104,4 +146,64 @@ func savePrivateKeyToFile(path string, keyBytes []byte) error {
 	}
 
 	return nil
+}
+
+// https://docs.freeipapi.com/
+func getIpInfo(ipAddress string) (*string, error) {
+	url := fmt.Sprintf("https://freeipapi.com/api/json/%s", ipAddress)
+
+	response, err := http.Get(url)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	if response.StatusCode != 200 {
+		err = fmt.Errorf("status code %s", response.Status)
+		log.Println(err)
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	stringResult := string(body)
+	return &stringResult, nil
+}
+
+type Connection struct {
+	ID uint64 `json:"id" gorm:"primarykey"`
+
+	IPVersion     int     `json:"ipVersion"`
+	IPAddress     string  `json:"ipAddress"`
+	Latitude      float64 `json:"latitude"`
+	Longitude     float64 `json:"longitude"`
+	CountryName   string  `json:"countryName"`
+	CountryCode   string  `json:"countryCode"`
+	TimeZone      string  `json:"timeZone"`
+	ZipCode       string  `json:"zipCode"`
+	CityName      string  `json:"cityName"`
+	RegionName    string  `json:"regionName"`
+	IsProxy       bool    `json:"isProxy"`
+	Continent     string  `json:"continent"`
+	ContinentCode string  `json:"continentCode"`
+
+	User      string    `json:"user"`
+	Password  string    `json:"password"`
+	Timestamp time.Time `json:"timestamp"`
+}
+
+func NewConnection(jsonData string, user string, password string, timestamp time.Time) (*Connection, error) {
+	var connection Connection
+	err := json.Unmarshal([]byte(jsonData), &connection)
+	if err != nil {
+		return nil, err
+	}
+	connection.User = user
+	connection.Password = password
+	connection.Timestamp = timestamp
+	return &connection, nil
 }
